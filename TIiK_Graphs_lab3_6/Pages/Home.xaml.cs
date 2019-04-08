6e9eb335fc671956f0c00488259cfa88215c64cc
Rodion <Radium-ON@outlook.com>
2019-04-08 23:46:40 +0700

    Добавьте файлы проекта.

---
+++ TIiK_Graphs_lab3_6/Pages/Home.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows.Controls;
+
+namespace TIiK_Graphs_lab3_6.Pages
+{
+
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        private Regex regex = new Regex(@"^[0-9]*\,?[0-9]*$");
+
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
