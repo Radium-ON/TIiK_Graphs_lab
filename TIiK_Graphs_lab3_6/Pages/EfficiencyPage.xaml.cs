88825826a877c3eb824e34f39d0dbcea37dd49fd
Rodion <Radium-ON@outlook.com>
2019-04-09 23:33:27 +0700

    IContent implementation

--- TIiK_Graphs_lab3_6/Pages/EfficiencyPage.xaml.cs
+++ TIiK_Graphs_lab3_6/Pages/EfficiencyPage.xaml.cs
@@ -12,17 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows;
+using FragmentNavigationEventArgs = FirstFloor.ModernUI.Windows.Navigation.FragmentNavigationEventArgs;
+using NavigatingCancelEventArgs = FirstFloor.ModernUI.Windows.Navigation.NavigatingCancelEventArgs;
+using NavigationEventArgs = FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs;
 
 namespace TIiK_Graphs_lab3_6.Pages
 {
     /// <summary>
     /// Interaction logic for EfficiencyPage.xaml
     /// </summary>
-    public partial class EfficiencyPage : UserControl
+    public partial class EfficiencyPage : UserControl, IContent
     {
         public EfficiencyPage()
         {
             InitializeComponent();
         }
+
+        #region Implementation of IContent
+
+        public void OnFragmentNavigation(FragmentNavigationEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
--- TIiK_Graphs_lab3_6/Pages/MatrixPage.xaml.cs
+++ TIiK_Graphs_lab3_6/Pages/MatrixPage.xaml.cs
@@ -14,7 +14,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows;
 using FirstFloor.ModernUI.Windows.Controls;
+using FragmentNavigationEventArgs = FirstFloor.ModernUI.Windows.Navigation.FragmentNavigationEventArgs;
+using NavigatingCancelEventArgs = FirstFloor.ModernUI.Windows.Navigation.NavigatingCancelEventArgs;
+using NavigationEventArgs = FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs;
 
 namespace TIiK_Graphs_lab3_6.Pages
 {
@@ -22,7 +26,7 @@ namespace TIiK_Graphs_lab3_6.Pages
     /// <summary>
     /// Interaction logic for Home.xaml
     /// </summary>
-    public partial class Home : UserControl
+    public partial class Home : UserControl, IContent
     {
         private Regex regex = new Regex(@"^[0-9]*\,?[0-9]*$");
 
@@ -31,5 +35,29 @@ namespace TIiK_Graphs_lab3_6.Pages
         {
             InitializeComponent();
         }
+
+        #region Implementation of IContent
+
+        public void OnFragmentNavigation(FragmentNavigationEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
