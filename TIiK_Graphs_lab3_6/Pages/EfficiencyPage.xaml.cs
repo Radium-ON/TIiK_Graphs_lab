356084a388bd4e5b0d0d661466667997eaa55248
Rodion <Radium-ON@outlook.com>
2019-04-09 19:31:56 +0700

    Add TabPage and efficiency page

---
+++ TIiK_Graphs_lab3_6/Pages/EfficiencyPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TIiK_Graphs_lab3_6.Pages
+{
+    /// <summary>
+    /// Interaction logic for EfficiencyPage.xaml
+    /// </summary>
+    public partial class EfficiencyPage : UserControl
+    {
+        public EfficiencyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
