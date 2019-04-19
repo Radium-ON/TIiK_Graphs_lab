632cd49f8b94374608da248fd1bfc4a643f0f048
Rodion <Radium-ON@outlook.com>
2019-04-19 09:43:04 +0700

    Add Model; add ViewModels; implement Prism for VMs & Model

---
+++ TIiK_Graphs_lab3_6/Models/VertexNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace TIiK_Graphs_lab3_6.Models
+{
+    public class VertexNode : BindableBase
+    {
+        public int VertexId { get; set; }
+
+        public int ParentId { get; set; }
+
+        public int Distance { get; set; }
+
+        public string Name { get; set; }
+
+        public int VStatus { get; set; }
+
+        public enum VertexStatusEnum
+        {
+            noVisited, visited, closed
+        }
+    }
+}
---
+++ TIiK_Graphs_lab3_6/ViewModels/EfficiencyPageVM.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIiK_Graphs_lab3_6.ViewModels
+{
+    class EfficiencyPageVM : MainWindowVM
+    {
+    }
+}
