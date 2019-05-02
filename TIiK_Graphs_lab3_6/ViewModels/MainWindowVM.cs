a2ae818a58c7ffe4533f6b8685973149c26f573d
Rodion <Radium-ON@outlook.com>
2019-05-02 12:26:18 +0700

    Новый конвертер для нумерации строк таблицы (работает).

--- TIiK_Graphs_lab3_6/ViewModels/MainWindowVM.cs
+++ TIiK_Graphs_lab3_6/ViewModels/MainWindowVM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -6,8 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Data;
-using FirstFloor.ModernUI.Windows.Controls;
-using Prism.Mvvm;
 using TIiK_Graphs_lab3_6.Models;
 
 namespace TIiK_Graphs_lab3_6.ViewModels
@@ -19,23 +19,25 @@ namespace TIiK_Graphs_lab3_6.ViewModels
             //_matrixPageVm = new MatrixPageVM();
             //_efficiencyPageVm = new EfficiencyPageVM();
             MatrixAdjacency = VertexFactory.GetWeightMatrixCost();
-            for(var i=0; i<MatrixAdjacency.Count;i++)
+
+            for (var i = 0; i < MatrixAdjacency.Count; i++)
             {
                 ColumnCollection.Add(
                     new FirstFloor.ModernUI.Windows.Controls.DataGridTextColumn()
                     {
                         Binding = new Binding($"[{i}]"),
-                        Header = i+1,
+                        Header = i + 1,
                     });
             }
-
         }
 
         private ObservableCollection<ObservableCollection<int>> _matrixAdjaency;
-        public ObservableCollection <ObservableCollection<int>> MatrixAdjacency
+        public ObservableCollection<ObservableCollection<int>> MatrixAdjacency
         {
-            get { return _matrixAdjaency;}
-            set {_matrixAdjaency = value ;
+            get { return _matrixAdjaency; }
+            set
+            {
+                _matrixAdjaency = value;
                 RaisePropertyChanged("MatrixAdjacency");
             }
         }
---
+++ TIiK_Graphs_lab3_6/ViewModels/RowNumberConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace TIiK_Graphs_lab3_6.ViewModels
+{
+    internal class RowNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DataGridRow row = value as DataGridRow;
+            if (row == null)
+                throw new InvalidOperationException("Этот конвертер может использоваться только с элементами DataGridRow");
+ 
+            return row.GetIndex() + 1;
+        }
+ 
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+}
